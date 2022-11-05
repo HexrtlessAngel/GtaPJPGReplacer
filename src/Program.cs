6188e4426ff67b4f3c54aeeb6094f885942df083
HexrtlessAngel <95123544+HexrtlessAngel@users.noreply.github.com>
2022-11-05 06:29:15 -0400

    semi-functional code
    I pulled it off! Fuck yes!

--- src/Program.cs
+++ src/Program.cs
@@ -9,9 +9,39 @@ namespace GtaPJPGReplacer
 {
     static class Program
     {
+        private const int fileSize = 528192;
+        private static byte[] outputData;
+
         static void Main(string[] args)
         {
+            try
+            {
+                int offset = 0x124;
 
+                string mainFilePath = $"{Directory.GetFiles(Environment.CurrentDirectory, "PGTA5*")[0]}";
+                string imgJpgPath = $"{Directory.GetFiles(Environment.CurrentDirectory, "*.jpg")[0]}";
+
+                Console.WriteLine(mainFilePath);
+                Console.WriteLine(imgJpgPath);
+
+                byte[] fileBytes = File.ReadAllBytes(mainFilePath);
+                Console.WriteLine($"Size of {Path.GetFileName(mainFilePath)}: {fileBytes.Length} bytes");
+                byte[] imgJpgBytes = File.ReadAllBytes(imgJpgPath);
+                Console.WriteLine($"Size of {Path.GetFileName(imgJpgPath)}: {imgJpgBytes.Length} bytes");
+
+                for (int i = 0; i < imgJpgBytes.Length; i++)
+                {
+                    fileBytes[offset + i] = imgJpgBytes[i];
+                }
+
+                File.WriteAllBytes($"{Directory.GetFiles(Environment.CurrentDirectory, "PGTA5*")[0]}", fileBytes);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.GetType().Name}\n{e.Message}\n{e.StackTrace}");
+            }
+
+            Console.ReadKey();
         }
     }
 }
