f5496a19b50c07429b55340d7f226d124b63a0db
HexrtlessAngel <95123544+HexrtlessAngel@users.noreply.github.com>
2022-11-05 04:02:13 -0400

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GtaPJPGReplacer.src
+namespace GtaPJPGReplacer
 {
-    internal class Program
+    static class Program
     {
+        static void Main(string[] args)
+        {
+
+        }
     }
 }
